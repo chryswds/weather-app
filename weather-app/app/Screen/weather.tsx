ab1ad28374e0837a3d5b493a82c7fd597c670605
2023154 <2023154@student.cct.ie>
2025-05-14 08:57:35 +0100

    #2 - config: Getting the API from a website.
    Here i access i a website to get this API, i had to logging into the website to get the weather API.

--- weather-app/app/Screen/weather.tsx
+++ weather-app/app/Screen/weather.tsx
@@ -1,6 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+
+//here i have got a API from this website below, and im a going to use in the weather aplication.
+
+//API website link. https://home.openweathermap.org/api_keys
+//IP key 44d5404359ef466582d9af9646eaad70
+//API link to use in the application.
+//https://api.openweathermap.org/data/2.5/weather?lat=53.3441204&lon=6.2673368&appid=44d5404359ef466582d9af9646eaad70&units=metric
+
+
+
 const weatherScreen = () => {
   return (
     <View>
